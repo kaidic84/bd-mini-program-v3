07b4b6b1ae54f3be6dab5103791d6b66bc6232e7
kaidic84 <jackson20000804@gmail.com>
2026-02-03 18:35:51 +0800

    bug调试

--- src/pages/projects/ProjectNew.tsx
+++ src/pages/projects/ProjectNew.tsx
@@ -307,7 +307,7 @@ const ProjectNew: React.FC = () => {
               />
             </div>
             <div className="space-y-2">
-              <Label>AI策略 *</Label>
+              <Label>BD *</Label>
               <Select value={bd} onValueChange={setBd}>
                 <SelectTrigger>
                   <SelectValue placeholder="请选择" />
